c19c8fc428ee2c3d322b1e4f18122290d656ede4
Mike <nguyen.mike1092@gmail.com>
2024-02-22 18:04:22 -0500

    added api/user routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const apiRouter = require('./api');
 
 router.use('./api', apiRouter);
+router.use((req, res) => res.send('Wrong route!'));
 
 module.exports = router
