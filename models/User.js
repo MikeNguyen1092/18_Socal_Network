0271a14e2fd642d85d5f3e70e0f2269fe9c3b4a5
Mike <nguyen.mike1092@gmail.com>
2024-02-22 19:08:20 -0500

    added thoughts and friends to user models

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const {Schema, model } = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
 	username: {
 		type: String,
 		required: true,
@@ -12,9 +12,21 @@ const userSchema = new mongoose.Schema({
     match: [/.+@.+\..+/, 'Must match an email address!'],
 		required: true,
 		unique: true,
-  }
+  },
+	thoughts: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'thought'
+		}
+	],
+	friends: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'user'
+		}
+	]
 });
 
-const User = model('User', userSchema)
+const User = model('user', userSchema)
 
 module.exports = User;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,18 +1,12 @@
 const router = require('express').Router();
-const {
-  getUsers,
-  getSingleUser,
-  createUser,
-  deleteUser,
-  add
-} = require('../../controllers/userController')
+const { getUsers, getSingleUser, createUser, deleteUser, updateUser } = require('../../controllers/userController');
 
+// /api/users
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser).delete(deleteUser)
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
-
-
-
+router.route('/users/:userId/friends/:friendId').get(getSingleUser).post();
 
 module.exports = router;
