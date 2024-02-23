08960ba17fbec13daa08332c3d9352e00e65f66d
Mike <nguyen.mike1092@gmail.com>
2024-02-22 19:19:31 -0500

    added virtuals

--- models/User.js
+++ models/User.js
@@ -1,32 +1,44 @@
-const {Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			match: [/.+@.+\..+/, 'Must match an email address!'],
+			required: true,
+			unique: true,
+		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'thought',
+			},
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'user',
+			},
+		],
 	},
-  email: {
-    type: String,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-		required: true,
-		unique: true,
-  },
-	thoughts: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'thought'
-		}
-	],
-	friends: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'user'
-		}
-	]
-});
+	{
+		toJSON:{
+			virtuals: true
+		},
+		id: false,
+	}
+);
 
-const User = model('user', userSchema)
+userSchema.virtual('friendCount').get(function (){
+	return this.friends.length;
+})
+
+const User = model('user', userSchema);
 
 module.exports = User;
