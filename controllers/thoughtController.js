83699d79c2ba694fd5eaebca4ae873afa23657a7
Mike <nguyen.mike1092@gmail.com>
2024-02-28 19:35:27 -0500

    fixed reaction delete

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -98,12 +98,12 @@ module.exports = {
 	// delete reaction
 	async removeReaction(req, res) {
 		try {
-			const thought = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { runValidators: true, new: true });
+			const thought = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { _id: req.params.reactionId } } }, { runValidators: true, new: true });
 
 			return res.json(thought);
 		} catch (err) {
 			console.log(err);
-			return res.status(500).json({ message: 'Could not add reaction' }, err);
+			return res.status(500).json({ message: 'Could not delete reaction' }, err);
 		}
 	},
 };
